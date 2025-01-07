1ece05bd979673fdb6c93af37038164ad159c0a4
narandza <60319154+narandza@users.noreply.github.com>
2025-01-07 23:20:21 +0100

    imporve navbar formating; create navbar links

--- src/app/(main)/navbar.tsx
+++ src/app/(main)/navbar.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <nav>
-      <div className="">
-        <Image src="/logo.svg" alt="Logo" width={80} height={15} />
-      </div>
+    <nav className="flex items-center justify-center mt-2">
+      {/* TODO: Add dimensions to constants */}
+      <Link href="/">
+        <Image
+          src="/logo.svg"
+          alt="Logo"
+          width={160}
+          height={30}
+          className="cursor-pointer mr-4"
+        />
+      </Link>
+      <div className=""></div>
     </nav>
   );
 };
---
+++ src/constants/navbar-links.ts
@@ -0,0 +1,27 @@
+type NavbarLink = {
+  title: string;
+  href: string;
+};
+
+export const NavbarLinks: NavbarLink[] = [
+  {
+    title: "Home",
+    href: "#top",
+  },
+  {
+    title: "About me",
+    href: "#about",
+  },
+  {
+    title: "Services",
+    href: "#services",
+  },
+  {
+    title: "My Work",
+    href: "#work",
+  },
+  {
+    title: "Contact me",
+    href: "#contact",
+  },
+];
