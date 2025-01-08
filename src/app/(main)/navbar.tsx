f668b1153be0a8bfc54a7ece6a0d1420771aa430
narandza <60319154+narandza@users.noreply.github.com>
2025-01-08 08:38:01 +0100

    create contact button; fixed styling

--- src/app/(main)/navbar.tsx
+++ src/app/(main)/navbar.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import { NAVBAR_LINKS as links } from "@/constants/navbar-links";
+import { ArrowUpRight } from "lucide-react";
 
 export const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between px-4 mt-2">
+    <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 z-50 flex items-center justify-between">
       {/* TODO: Add dimensions to constants */}
-      <Link href="/">
+      <Link href="#top">
         <Image
           src="/logo.svg"
           alt="Logo"
@@ -15,14 +16,22 @@ export const Navbar = () => {
           className="ml-4"
         />
       </Link>
-      <div className="absolute left-1/2 transform -translate-x-1/2 flex gap-x-5">
+      <div className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3">
         {links.map((link, index) => (
-          <Link key={index} href={link.href} className="text-2xl">
+          <Link key={index} href={link.href} className="text-xl">
             {link.title}
           </Link>
         ))}
       </div>
-      <div className=""></div>
+      <div className=" ">
+        <Link
+          href="#contact"
+          className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
+        >
+          Contact
+          <ArrowUpRight className="size-5 ml-2" />
+        </Link>
+      </div>
     </nav>
   );
 };
