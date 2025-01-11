99c6468475baf99b07d74fdd55eeb451f135bf63
narandza <60319154+narandza@users.noreply.github.com>
2025-01-11 23:58:07 +0100

    add cta buttons, add stylign

--- src/app/(main)/header.tsx
+++ src/app/(main)/header.tsx
@@ -1,9 +1,11 @@
+import { ArrowRightIcon, DownloadIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="pt-16">
-      <div className="pt-16">
+    <header className="pt-[220px]">
+      <div className="mx-auto">
         <Image
           src="/placeholder-image.png"
           alt="Profile Image"
@@ -11,17 +13,33 @@ export const Header = () => {
           height={32}
           className="rounded-full"
         />
+
         <h3 className="text-xl md:text-2xl mb-3">
           Hi! I&apos;m Dimitrije Jovanovic
         </h3>
 
         <h1 className="text-3xl sm:tet-6xl lg:text-[66px] ">
-          frontend web developer base in Belgrade.
+          frontend web developer based in Belgrade.
         </h1>
         <p className="max-w-2xl mx-auto">
           I&apos;m a frontend developer from Belgrade, Serbia with 3+ years of
           experience
         </p>
+      </div>
+      <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
+        <Link
+          href="#contact"
+          className="px-10 py-3 border rounded-full border-white bg-foreground text-background flex items-center gap-2"
+        >
+          Contact me <ArrowRightIcon />
+        </Link>
+        {/* TODO: Add resume */}
+        <Link
+          href=""
+          className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
+        >
+          My Resume <DownloadIcon />
+        </Link>
       </div>
     </header>
   );
