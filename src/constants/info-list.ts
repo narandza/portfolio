dae499b57611b75ccac96afe306ad3388455a59a
narandza <60319154+narandza@users.noreply.github.com>
2025-01-13 11:10:52 +0100

    create info list asset

---
+++ src/constants/info-list.ts
@@ -0,0 +1,23 @@
+type InfoList = {
+  icon: string;
+  title: string;
+  description: string;
+};
+
+export const InfoList: InfoList[] = [
+  {
+    icon: "",
+    title: "Languages",
+    description: "HTML, CSS, JavaScript, ReactJs, NextJs",
+  },
+  {
+    icon: "",
+    title: "Education",
+    description: "Computer Science",
+  },
+  {
+    icon: "",
+    title: "Projects",
+    description: "Build 10+ projects",
+  },
+];
