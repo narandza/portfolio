af14f7c2a0a92b27e69c5fc96f870bdae18dd2d7
narandza <60319154+narandza@users.noreply.github.com>
2025-01-16 18:02:21 +0100

    add service items

--- src/app/(main)/services.tsx
+++ src/app/(main)/services.tsx
@@ -1,3 +1,7 @@
+import { serviceData } from "@/constants/info-list";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
 export const Services = () => {
   return (
     <div id="services" className="w-full px-[12%] py-10 scroll-mt-20 ">
@@ -8,6 +12,22 @@ export const Services = () => {
         I am a frontend developer from Belgrade, Serbia with 3+ years of
         experience
       </p>
+
+      <div className="grid grid-cols-4 gap-6 my-10 place-items-center">
+        {serviceData.map(({ icon: Icon, title, description, link }, index) => (
+          <div className="w-full" key={index}>
+            <Icon className="w-10" />
+            <h3 className="text-xl my-4">{title}</h3>
+            <p className="text-sm leading-5">{description}</p>
+            <Link
+              href={link}
+              className="flex items-center gap-1 text-muted-foreground"
+            >
+              Read more <ArrowRight className="size-4" />
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
