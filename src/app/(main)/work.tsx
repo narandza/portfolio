64d9c6c2d6e9831836e528d4cf1a10da7e9bcc8c
narandza <60319154+narandza@users.noreply.github.com>
2025-01-17 15:52:03 +0100

    create work component

---
+++ src/app/(main)/work.tsx
@@ -0,0 +1,9 @@
+export const Work = () => {
+  return (
+    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg "></h4>
+      <h2 className="text-center text-5xl "></h2>
+      <p className="text-center max-2-2xl mx-auto mt-5 mb-12"></p>
+    </div>
+  );
+};
