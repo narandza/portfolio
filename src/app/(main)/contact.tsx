9dc65865cd498fa5316a33ff5eca783a215be1da
narandza <60319154+narandza@users.noreply.github.com>
2025-01-18 20:14:24 +0100

    add shadcn input component

--- src/app/(main)/contact.tsx
+++ src/app/(main)/contact.tsx
@@ -3,7 +3,10 @@ export const Contact = () => {
     <div className="w-full px-[12%] py-10 scroll-mt-20" id="contact">
       <h4 className="text-center mb-2 text-lg">Connect with me</h4>
       <h2 className="text-center text-5xl">Get in touch</h2>
-      <p className="text-center max-w-2 mx-auto mt-5 mb-12"></p>
+      <p className="text-center max-w-2 mx-auto mt-5 mb-12">
+        I&apos;d love to hear from you! If you have any questions, comments, or
+        feedback, please use the form below
+      </p>
     </div>
   );
 };
