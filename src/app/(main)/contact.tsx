aad6a2f511e78b9c8c73363583889b365e679a16
narandza <60319154+narandza@users.noreply.github.com>
2025-01-19 19:26:30 +0100

    Add form styling

--- src/app/(main)/contact.tsx
+++ src/app/(main)/contact.tsx
@@ -13,6 +13,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 
 const formSchema = z.object({
   name: z.string().min(2, { message: "Must be 2 or more characters long" }),
@@ -23,8 +24,14 @@ const formSchema = z.object({
 export const Contact = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
   });
-
+  // TODO: Add sonner
+  // TODO: Add functionality that sends the message to my email
   const onSubmit = (data: z.infer<typeof formSchema>) => {
     console.log(data);
   };
@@ -39,35 +46,41 @@ export const Contact = () => {
       </p>
 
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="John Doe" {...field} />
-                </FormControl>
-                <FormDescription>Enter your name</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="example@mail.com" {...field} />
-                </FormControl>
-                <FormDescription>Enter your email</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 max-w-2xl mx-auto"
+        >
+          <div className="grid grid-cols-auto lg:grid-cols-2 gap-6 mt-10 mb-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="John Doe" {...field} />
+                  </FormControl>
+                  <FormDescription>Enter your name</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="example@mail.com" {...field} />
+                  </FormControl>
+                  <FormDescription>Enter your email</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
           <FormField
             control={form.control}
             name="message"
@@ -81,7 +94,14 @@ export const Contact = () => {
               </FormItem>
             )}
           />
-          <Button variant="ghost">Submit</Button>
+          {/* TODO: Replace all buttons with btn outline */}
+          <Button
+            variant="outline"
+            className="flex items-center justify-between gap-2 rounded-full mx-auto py-3 px-8"
+          >
+            Submit
+            <ArrowRight className="size-4" />
+          </Button>
         </form>
       </Form>
     </div>
