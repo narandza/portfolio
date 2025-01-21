3e02c22e12795c9ae4d13c4812c4d466e173ef2b
narandza <60319154+narandza@users.noreply.github.com>
2025-01-21 18:38:15 +0100

    add constasnts in footer; make year dynamic;

--- src/app/(main)/footer.tsx
+++ src/app/(main)/footer.tsx
@@ -3,15 +3,17 @@ import { MailIcon } from "lucide-react";
 
 import { Logo } from "@/components/logo";
 import { SocialMedia } from "@/components/social-media";
+import { LOGO_HEIGHT, LOGO_WIDTH } from "@/constants/image-dimensions";
+
+const currentYear = new Date().getFullYear();
+const iconSize = 20;
 
 export const Footer = () => {
   return (
     <footer className="mt-20">
       <div className="flex flex-col items-center justify-center">
-        {/* TODO: Reformat imports on all pages */}
-        {/* TODO: Make logo component, replace in navbar also */}
         <Link href="#top" className="pb-1">
-          <Logo width={160} height={30} />
+          <Logo width={LOGO_WIDTH} height={LOGO_HEIGHT} />
         </Link>
 
         <div className="w-max flex items-center gap-2 mx-auto">
@@ -21,12 +23,11 @@ export const Footer = () => {
       </div>
 
       <div className="sm:flex items-center justify-between mt-12 mx-[10%] border-t border-muted-foreground py-6">
-        {/* TODO: Make year dynamic */}
         <p className="">
-          &copy; 2025 Dimitrije Jovanovic. All rights reserved.
+          &copy; {currentYear} Dimitrije Jovanovic. All rights reserved.
         </p>
         <div className="flex gap-10 items-center justify-center mt-4 sm:mt-0">
-          <SocialMedia iconSize={20} />
+          <SocialMedia iconSize={iconSize} />
         </div>
       </div>
     </footer>
