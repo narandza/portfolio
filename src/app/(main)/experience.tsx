a40c3d5af233b17542152d709a446151990756c5
narandza <60319154+narandza@users.noreply.github.com>
2025-01-25 21:37:25 +0100

    animate experience heading

--- src/app/(main)/experience.tsx
+++ src/app/(main)/experience.tsx
@@ -1,12 +1,39 @@
+import { motion } from "motion/react";
+
 export const Experience = () => {
   return (
-    <div id="experience" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg">Professional Journey</h4>
-      <h2 className="text-center text-5xl">Where I&apos;ve Made an Impact</h2>
-      <p className="text-center max-w-2xl mx-auto mt-5 mb-12">
+    <motion.div
+      id="experience"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <motion.h4
+        className="text-center mb-2 text-lg"
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, delay: 0.5 }}
+      >
+        Professional Journey
+      </motion.h4>
+      <motion.h2
+        className="text-center text-5xl"
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+      >
+        Where I&apos;ve Made an Impact
+      </motion.h2>
+      <motion.p
+        className="text-center max-w-2xl mx-auto mt-5 mb-12"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.7 }}
+      >
         Explore the milestones of my career, highlighting the roles and projects
         where I contributed, learned, and grew as a professional.
-      </p>
-    </div>
+      </motion.p>
+    </motion.div>
   );
 };
